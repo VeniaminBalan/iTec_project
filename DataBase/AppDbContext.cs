919778a5bd01acfd26040423b807cfdfdc39e839
Balan Veniamin <veniamin.balan02@gmail.com>
2023-04-02 04:15:32 +0300

    sdd

--- DataBase/AppDbContext.cs
+++ DataBase/AppDbContext.cs
@@ -1,7 +1,10 @@
-﻿using iTec_project.Features.Profiles.Models;
+﻿using System.Diagnostics;
+using iTec_project.Features.Profiles.Models;
 using iTec_project.Features.Users.Models;
 using Microsoft.EntityFrameworkCore;
 using iTec_project.Features.Roles.Models;
+using iTec_project.Features.Profiles.Models;
+using MySql.EntityFrameworkCore.Extensions;
 
 namespace iTec_project.DataBase;
 
@@ -12,4 +15,21 @@ public class AppDbContext : DbContext
     public DbSet<UserModel> Users { get; set;}
     public DbSet<RoleModel> Roles { get; set;}
     public DbSet<ProfileModel> Profiles { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        /*
+        modelBuilder.Entity<ProfileModel>(builder =>
+        {
+            if (Database.IsMySql())
+            {
+                builder.Property(p => p.BackendFramework)
+                    .HasConversion(
+                        v => v.ToString(),
+                        v => 
+            }
+        });*/
+        
+        base.OnModelCreating(modelBuilder);
+    }
 }
